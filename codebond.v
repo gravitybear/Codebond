261b521e43f01c3ff2a180d2c440362c33cf2377
Yu Geng Wang <wangy190@IITS-I402-12.utsc-labs.utoronto.ca>
2017-03-29 21:49:50 -0400

    completed project

--- codebond.v
+++ codebond.v
@@ -1,7 +1,8 @@
-module Project(KEY, HEX5, HEX4, HEX2, HEX1, HEX0, SW, LEDR, LEDG, CLOCK_50);
+module Project(KEY, HEX5, HEX4, HEX2, HEX1, HEX0, SW, LEDR, LEDG, GPIO, CLOCK_50);
 	input [3:0] KEY;
 	input [17:0] SW;
 	input CLOCK_50;
+	input [2:0] GPIO;
 	
 	output [4:0] LEDG;
 	output [17:0] LEDR;
@@ -14,11 +15,13 @@ module Project(KEY, HEX5, HEX4, HEX2, HEX1, HEX0, SW, LEDR, LEDG, CLOCK_50);
 
 	 // the time enable once per second
 	reg [25:0] En = 0;
-	wire Enable = (En == 0)&& (~(ten == 4'b0110) || SW[0]) && ~winner; // assuming ~winner works...........
+	wire Enable = (En == 0)&& (~(ten == 4'b0110) || SW[0]) && ~(winner || gameover); // assuming ~winner works...........
 	always @(posedge CLOCK_50) begin
 		En <= (Enable ? 50_000_000 : En) - 1;
 	end
-
+	
+	wire [1:0] a, b, c;
+	wire [1:0] num_inputs;
 
 	wire [2:0] randVal1;
 	wire [2:0] randVal2;
@@ -28,7 +31,6 @@ module Project(KEY, HEX5, HEX4, HEX2, HEX1, HEX0, SW, LEDR, LEDG, CLOCK_50);
 	wire [1:0] inOne;
 	wire [1:0] inTwo;	
 	wire [1:0] inThree;
-
 	
 
 	lfsr rand1(
@@ -60,9 +62,12 @@ module Project(KEY, HEX5, HEX4, HEX2, HEX1, HEX0, SW, LEDR, LEDG, CLOCK_50);
 	hex_decoder h0(randVal1%3, HEX0);
 	hex_decoder h1(randVal2%3, HEX1);
 	hex_decoder h2(randVal3%3, HEX2);
+	/*
+	hex_decoder h0(inOne, HEX0);
+	hex_decoder h1(inTwo, HEX1);
+	hex_decoder h2(inThree, HEX2);*/
 
-
-   	counter Q0(.enable(KEY[3]),.clock(Enable),.clear_b(~SW[0]),.q(one[3:0]));
+   counter Q0(.enable(~(winner || gameover)),.clock(Enable),.clear_b(~SW[0]),.q(one[3:0]));
 	
 	assign en_q1 = (one == 4'b1001) ? 1 : 0; //if one == 9 then ten + 1;
 	counter2 Q1(.enable(en_q1),.clock(Enable),.clear_b(~SW[0]),.q(ten[3:0]));
@@ -70,17 +75,22 @@ module Project(KEY, HEX5, HEX4, HEX2, HEX1, HEX0, SW, LEDR, LEDG, CLOCK_50);
   	hex_decoder h4(one[3:0] , HEX4);
 	hex_decoder h5(ten[3:0] , HEX5);
 	
-	
-	
-	assign inOne = SW[17:16];
+	assign inOne = SW[13:12];
 	assign inTwo = SW[15:14];
-	assign inThree = SW[13:12];
+	assign inThree = SW[17:16];
 	
-	assign LEDR[17:16] = (inOne == randVal1%3);
+	assign LEDR[13:12] = (inOne == randVal1%3);
 	assign LEDR[15:14] = (inTwo == randVal2%3);
-	assign LEDR[13:12] = (inThree == randVal3%3);
+	assign LEDR[17:16] = (inThree == randVal3%3);
 
 	assign winner = ((inOne == randVal1%3) && (inTwo == randVal2%3) && (inThree == randVal3%3));
+	assign gameover = (ten == 4'b0101 && one == 4'b1001);
+	
+	assign LEDG[4:0] = winner;
+	
+	/*buttonPress dsda(
+		.GPIO(GPIO[2:0]), .a(a[1:0]), .b(b[1:0]), .c(c[1:0]), .num_inputs(num_inputs), .clock(CLOCK_50)); 
+		*/
 
 endmodule
 
@@ -134,6 +144,132 @@ module lfsr(
 				 out[0], linear_feedback};
 	end
 endmodule
+
+/*
+module buttonPress(GPIO, ld_a, ld_b, ld_c, num_inputs, clock);
+	input clock;
+	input [2:0] GPIO;
+	output reg a, b, c, num_inputs;
+	
+	 localparam  S_LOAD_A        = 5'd0,
+                S_LOAD_A_WAIT   = 5'd1,
+                S_LOAD_B        = 5'd2,
+                S_LOAD_B_WAIT   = 5'd3,
+                S_LOAD_C        = 5'd4,
+                S_LOAD_C_WAIT   = 5'd5,
+                S_CYCLE_0       = 5'd6,
+                S_CYCLE_1       = 5'd7,
+                S_CYCLE_2       = 5'd8,
+                S_CYCLE_3       = 5'd9,
+					 
+	always@(*)
+    begin: state_table 
+            case (current_state)
+				S_LOAD_A: next_state = go ? S_LOAD_A_WAIT : S_LOAD_A; // Loop in current state until value is input
+				 S_LOAD_A_WAIT: next_state = go ? S_LOAD_A_WAIT : S_LOAD_B; // Loop in current state until go signal goes low
+				 S_LOAD_B: next_state = go ? S_LOAD_B_WAIT : S_LOAD_B; // Loop in current state until value is input
+				 S_LOAD_B_WAIT: next_state = go ? S_LOAD_B_WAIT : S_LOAD_C; // Loop in current state until go signal goes low
+				 S_LOAD_C: next_state = go ? S_LOAD_C_WAIT : S_LOAD_C; // Loop in current state until value is input
+				 S_LOAD_C_WAIT: next_state = go ? S_LOAD_X_WAIT : S_CYCLE_0; // Loop in current state until go signal goes lo
+				 S_CYCLE_0: next_state = S_CYCLE_1;
+				 S_CYCLE_1: next_state = S_CYCLE_2; 
+				 S_CYCLE_2: next_state = S_CYCLE_3;
+				 S_CYCLE_3: next_state = S_LOAD_A;
+				default:     next_state = S_LOAD_A;
+        endcase
+    end // state_table
+	always @(*)
+    begin: enable_signals
+        // By default make all our signals 0
+        ld_alu_out = 1'b0;
+        ld_a = 1'b0;
+        ld_b = 1'b0;
+        ld_c = 1'b0;
+        ld_x = 1'b0;
+        ld_r = 1'b0;
+        alu_select_a = 2'b0;
+        alu_select_b = 2'b0;
+        alu_op       = 1'b0;
+
+        case (current_state)
+            S_LOAD_A: begin
+                ld_a = 1'b1;
+                end
+            S_LOAD_B: begin
+                ld_b = 1'b1;
+                end
+            S_LOAD_C: begin
+                ld_c = 1'b1;
+                end
+            S_CYCLE_0: begin // Do A * x
+                ld_alu_out = 1'b1; ld_a = 1'b1; // store result back into A
+                alu_select_a = 2'b00; // Select register A
+                alu_select_b = 2'b11; // Also select register x
+                alu_op = 1'b1; // multiply
+            end
+            S_CYCLE_1: begin // Do Ax * x
+                ld_alu_out = 1'b1; ld_a = 1'b1; // store result in result register
+					 //ld_x = 1'b1;
+                alu_select_a = 2'b00; // Select register Ax
+                alu_select_b = 2'b11; // Select register x
+                alu_op = 1'b1; // multiply
+            end
+            S_CYCLE_2: begin // Do B * x
+                ld_alu_out = 1'b1; ld_b = 1'b1; // store result in result registe
+					 //ld_x = 1'b1;
+                alu_select_a = 2'b01; // Select register B
+                alu_select_b = 2'b11;// Select register x
+                alu_op = 1'b1; // multiply
+            end
+            S_CYCLE_3: begin // Do (Ax^2) + Bx
+                ld_alu_out = 1'b1; ld_a = 1'b1; // store result in result register
+                alu_select_a = 2'b00; // Select register Ax^2
+                alu_select_b = 2'b01; // Select register Bx
+                alu_op = 1'b0; // add
+            end
+        // default:    // don't need default since we already made sure all of our outputs were assigned a value at the start of the always block
+        endcase
+    end // enable_signals
+   
+    // current_state registers
+    always@(posedge clock)
+    begin: state_FFs
+        if(!resetn)
+            current_state <= S_LOAD_A;
+        else
+            current_state <= next_state;
+    end // state_FFS
+	 
+   always @(posedge clock) // triggered every time clock rises
+	begin
+		if (num_inputs == 2'b00)
+			num_inputs <= 2'b01;
+			if (GPIO[0])
+				a <= 1'b11;
+			else if (GPIO[0])
+				a <= 1'b01;
+			else if (GPIO[0])
+				a <= 1'b10;
+		else if (num_inputs == 2'b01)
+			num_inputs <= 2'b10;
+			if (GPIO[1])
+				b <= 1'b11;
+			else if (GPIO[1])
+				b <= 1'b01;
+			else if (GPIO[1])
+				b <= 1'b10;
+		else if (num_inputs == 2'b10)
+			num_inputs <= 2'b00;
+			if (GPIO[2])
+				c <= 1'b11;
+			else if (GPIO[2])
+				c <= 1'b01;
+			else if (GPIO[2])
+				c <= 1'b10;
+			
+	end
+endmodule
+*/
 
 
 module counter(enable, clock, clear_b, q); //0-9 counter
